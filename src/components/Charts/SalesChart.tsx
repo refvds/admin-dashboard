cd15278405a82fc71bc0c7feed15c85742730588
refvds <refvdsk@gmail.com>
2024-09-04 15:25:05 -0700

    add sales chart

---
+++ src/components/Charts/SalesChart.tsx
@@ -0,0 +1,59 @@
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
+import { motion } from "framer-motion";
+import { FC } from "react";
+
+interface ISalesChart {
+  data: Array<any>;
+  xData: string;
+  yData: string;
+  title?: string;
+}
+
+const SalesChart: FC<ISalesChart> = ({ data, xData, yData, title }) => {
+  return (
+    <motion.article
+      className="bg-gray-800 bg-opacity-50 backdrop-blur-md shadow-lg border rounded-xl p-6 border-gray-700"
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ delay: 0.2 }}
+    >
+      {title && (
+        <h2 className="text-lg font-medium mb-4 text-gray-100">{title}</h2>
+      )}
+      <div className="h-80">
+        <ResponsiveContainer width="100%" height="100%">
+          <LineChart data={data}>
+            <CartesianGrid strokeDasharray="3 3" stroke="#4b5563" />
+            <XAxis dataKey={xData} />
+            <YAxis stroke="#9ca9af" />
+            <Tooltip
+              contentStyle={{
+                backgroundColor: "rgba(31, 41, 55, 0.8)",
+                borderColor: "#4b5563",
+              }}
+              itemStyle={{ color: "#e5e7eb" }}
+            />
+            <Line
+              type="monotone"
+              dataKey={yData}
+              stroke="#6366f1"
+              strokeWidth={3}
+              dot={{ fill: "#6366f1", strokeWidth: 2, r: 6 }}
+              activeDot={{ r: 8, strokeWidth: 2 }}
+            />
+          </LineChart>
+        </ResponsiveContainer>
+      </div>
+    </motion.article>
+  );
+};
+
+export default SalesChart;
