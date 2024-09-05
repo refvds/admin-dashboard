8e255e6ed7ed7bae4bf827eae618d8a7bc309f03
refvds <refvdsk@gmail.com>
2024-09-04 21:33:34 -0700

    add user table

--- src/constants.ts
+++ src/constants.ts
@@ -126,3 +126,12 @@ export const USER_GROWTH_DATA: Array<IUserGrowthItem> = [
 	{ month: "May", users: 4000 },
 	{ month: "Jun", users: 5000 },
 ]
+
+export const USER_DATA = [
+    { id: 1, name: "John Doe", email: "john@example.com", role: "Customer", status: "Active" },
+	{ id: 2, name: "Jane Smith", email: "jane@example.com", role: "Admin", status: "Active" },
+	{ id: 3, name: "Bob Johnson", email: "bob@example.com", role: "Customer", status: "Inactive" },
+	{ id: 4, name: "Alice Brown", email: "alice@example.com", role: "Customer", status: "Active" },
+	{ id: 5, name: "Charlie Wilson", email: "charlie@example.com", role: "Moderator", status: "Active" },
+
+] 
--- src/pages/UsersPage.tsx
+++ src/pages/UsersPage.tsx
@@ -1,11 +1,14 @@
 import GrowthChart from "../components/Charts/GrowthChart";
 import ChartWrapper from "../components/ui/ChartWrapper";
 import PageWrapper from "../components/ui/PageWrapper";
+import UserTabble from "../components/UserTabble";
 import { USER_GROWTH_DATA, USER_PAGE_STATS_DATA } from "../constants";
 
 const UsersPage = () => {
   return (
     <PageWrapper headerTitle="Users" stats={USER_PAGE_STATS_DATA}>
+      <UserTabble />
+
       <ChartWrapper title="User Growth">
         <GrowthChart data={USER_GROWTH_DATA} xData="month" yData="users" />
       </ChartWrapper>
